62ca37bf7d3db39142e398d1229a0f34915784cd
RoshanKumar13 <sroshan954@gmail.com>
2019-04-12 23:48:58 +0545

    success

---
+++ app/src/main/java/com/example/calculate_taxamount_rk/MainActivity.java
@@ -0,0 +1,50 @@
+package com.example.calculate_taxamount_rk;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText etAmount;
+    private Button btnCalculate;
+    private TextView tvOutput,txtAnnual;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        etAmount = findViewById(R.id.etAmount);
+        btnCalculate = findViewById(R.id.btnCalculate);
+        tvOutput = findViewById(R.id.tvOutput);
+        txtAnnual = findViewById(R.id.txtAnnual);
+
+        btnCalculate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                double amt = Double.parseDouble(etAmount.getText().toString());
+                double amount = amt*12;
+                txtAnnual.setText(Double.toString(amount));
+
+                calculate_Tax ta = new calculate_Tax();
+                ta.setAmount(amount);
+
+                if(amount<200000){
+                    double res = ta.lowTax();
+                    tvOutput.setText(Double.toString(res));
+                }else if(amount >=200000 && amt<350000){
+                    double res = ta.mediumTax();
+                    tvOutput.setText(Double.toString(res));
+                }else if(amount>=350000){
+                    double res = ta.highTax();
+                    tvOutput.setText(Double.toString(res));
+                }
+
+            }
+        });
+    }
+}
